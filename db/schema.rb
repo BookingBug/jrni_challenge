47b5a86b0e1789900562996674ca2d6403aec687
Richard Kettle <richardlkettle@gmail.com>
2018-12-04 14:36:01 +0000

    db schema for new company fields

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,9 @@ ActiveRecord::Schema.define(version: 20160711211414) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
+    t.text     "email"
     t.text     "description"
+    t.boolean  "disabled"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
