13e3754e867a470e067e56516f9c455b43f00512
Richard Kettle <richardlkettle@gmail.com>
2018-12-04 14:19:42 +0000

    added a couple of fields to the company csv

--- db/migrate/20160711211414_create_companies.rb
+++ db/migrate/20160711211414_create_companies.rb
@@ -2,7 +2,9 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :name
+      t.text :email
       t.text :description
+      t.boolean :disabled
       t.timestamps null: false
     end
   end
